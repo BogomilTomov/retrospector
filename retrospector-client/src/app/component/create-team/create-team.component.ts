73d3e6fb00794f631ef3ea4955ce8138a31d66cd
Bogomil Tomov <bogomil.tomov@mentormate.com>
2020-03-18 20:02:30 +0200

    q

--- retrospector-client/src/app/component/create-team/create-team.component.ts
+++ retrospector-client/src/app/component/create-team/create-team.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+//import { Team } from '../../team';
+import * as moment from 'moment';
+// import { TeamService } from '../team.service';
+// import { AccountsService } from '../services/accounts.service';
 
 @Component({
   selector: 'app-create-team',
@@ -6,10 +11,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-team.component.css']
 })
 export class CreateTeamComponent implements OnInit {
-
-  constructor() { }
+  public name: string = '';
+  constructor(private _teamService: TeamService,
+              private _accountService: AccountsService) { }
 
   ngOnInit(): void {
   }
 
-}
+  onSubmit(): void {
+    // let newTeam = new Team();
+    // newTeam.name = this.name;
+    // newTeam.creationDate = moment().add(2, 'h').toDate();
+    // newTeam.ownerId = localStorage.getItem('userId');
+    // this._teamService.createTeam(newTeam).subscribe();
+  }
