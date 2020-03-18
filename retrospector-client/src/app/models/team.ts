2fe969fabcbb301fdf4244ef6e81c07293f730af
Bogomil Tomov <bogomil.tomov@mentormate.com>
2020-03-18 20:06:19 +0200

    c

---
+++ retrospector-client/src/app/models/team.ts
@@ -0,0 +1,9 @@
+export class Team {
+    public name: string;
+    public creationDate: Date;
+    public ownerId: string;
+    //public sharedUsersIds: number[];
+    // public games: Game[];
+    constructor() {
+    }
+}
