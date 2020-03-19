ea3d17d8b2326f149f2e17ee2bca4aea773f5335
Bogomil Tomov <bogomil.tomov@mentormate.com>
2020-03-19 12:43:46 +0200

    Final comit - RET-6

--- retrospector-client/retrospector/Retrospector/Retrospector.Data/DomainModels/RetrospectorUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Retrospector.Data.DomainModels
-{
-    public class RetrospectorUser : IdentityUser
-    {
-
-    }
-}
--- retrospector-client/retrospector/retrospector-client/src/app/models/userViewModel.model.ts
+++
@@ -1,5 +0,0 @@
-import { SocialUser } from 'angularx-social-login';
-
-export class UserViewModel extends SocialUser {
-    role: string;
-}
