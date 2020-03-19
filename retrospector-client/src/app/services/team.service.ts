6e9e94b772024afd3d31ff1ab7b376986ac0de6c
Bogomil Tomov <bogomil.tomov@mentormate.com>
2020-03-19 07:26:07 +0200

    com

--- retrospector-client/src/app/services/team.service.ts
+++ retrospector-client/src/app/services/team.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Team } from '../models/team';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
+  teams: Team[] = [];
+  constructor(private _http: HttpClient) { 
+  }
 
-  constructor() { }
+  getTeams() {
+    var team = new Team();
+    team.name = "Team 1";
+    this.teams.push(team);
+    var team = new Team();
+    team.name = "Team 1";
+    this.teams.push(team);
+    var team = new Team();
+    team.name = "Team 1";
+    this.teams.push(team);
+    return this.teams;
+
+  }
+
+  createTeam(team: Team): Observable<Team> {
+    return this._http.post<Team>('https://localhost:44372/api/team',
+    team);
+  }
+
 }
